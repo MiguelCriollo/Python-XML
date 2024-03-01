1c914848bec560586b3231b1b1557d676262ea93
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2024-03-01 14:57:57 -0500

    Optimizacion de codigo, arreglo tablas

--- mainMenu.py
+++ mainMenu.py
@@ -6,19 +6,19 @@ import personasSancionadas
 
 class mainMenu:
     def __init__(self):
-        self.articulo1=personasSancionadas.personasSancionadas()
+        self.sanctionActions=personasSancionadas.personasSancionadas()
         self.ventana1=tk.Tk()
-        self.ventana1.title("Listas de Control onu -- ofac")
-        self.cuaderno1 = ttk.Notebook(self.ventana1)        
+        self.ventana1.title("Listas de Control ONU/OFAC")
+        self.cuaderno1 = ttk.Notebook(self.ventana1)    
+        self.listado_completo()  
         self.consulta_por_codigo()
-        self.listado_completo()
-        self.cuaderno1.grid(column=0, row=0, padx=100, pady=100)
+        self.cuaderno1.grid(column=0, row=0, padx=50, pady=50)
         self.ventana1.mainloop()
 
     def consulta_por_codigo(self):
-        self.pagina2 = ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina2, text="Consulta por ID")
-        self.labelframe1=ttk.LabelFrame(self.pagina2, text="Persona Sancionada:")
+        pagina2 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(pagina2, text="Consulta por ID")
+        self.labelframe1=ttk.LabelFrame(pagina2, text="Persona Sancionada:")
         self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
         self.label1=ttk.Label(self.labelframe1, text="Identificacion:")
         self.codigo=tk.StringVar()
@@ -29,9 +29,9 @@ class mainMenu:
         self.boton1.grid(column=1, row=3, padx=4, pady=4)
 
         # Pestaña de consulta por Nombre
-        self.pagina2 = ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina2, text="Consulta por Nombre")
-        self.labelframe2 = ttk.LabelFrame(self.pagina2, text="Persona Sancionada:")
+        pagina3 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(pagina3, text="Consulta por Nombre")
+        self.labelframe2 = ttk.LabelFrame(pagina3, text="Persona Sancionada:")
         self.labelframe2.grid(column=0, row=0, padx=5, pady=10)
         self.label2 = ttk.Label(self.labelframe2, text="Nombre:")
         self.codigo_nombre = tk.StringVar()
@@ -41,24 +41,40 @@ class mainMenu:
         self.boton2 = ttk.Button(self.labelframe2, text="Consultar por Nombre", command=lambda: self.consultar("NOMBRE"))
         self.boton2.grid(column=1, row=3, padx=4, pady=4)
 
+        pagina4 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(pagina4, text="Actualizar")
+        self.labelframe3 = ttk.LabelFrame(pagina4, text="Actualizar las listas de control:")
+        self.labelframe3.grid(column=0, row=0, padx=5, pady=10)
+        self.label3 = ttk.Label(self.labelframe3, text="Vuelve a recibir los datos conforme a la ultima actualizacion de la onu/ofac")
+        self.label3.grid(column=0, row=0, padx=4, pady=4)
+        self.boton3 = ttk.Button(self.labelframe3, text="Actualizar", command=lambda: self.sanctionActions.updateControlLists())
+        self.boton3.grid(column=1, row=3, padx=4, pady=4)
+
     def consultar(self,type):
         if(type=="ID"):
             datos=(self.codigo.get(), )
-            respuesta=self.articulo1.personLikeIdentification(datos)
+            respuesta=self.sanctionActions.personLikeIdentification(datos)
         else:
             datos=(self.codigo_nombre.get(), )
-            print("DATOS=>",datos)
-            respuesta=self.articulo1.personLikeName(datos)
+            respuesta=self.sanctionActions.personLikeName(datos)
         
-        #print('JSJSJS=>',self.articulo1.personLikeIdentification(datos))
+        #print('JSJSJS=>',self.sanctionActions.personLikeIdentification(datos))
         if respuesta:
             #print("Fila seleccionada:", respuesta)
             # Crear una ventana
             ventana_resultado = tk.Toplevel()
+            ventana_resultado.geometry("1000x400") 
             ventana_resultado.title("Resultados de la consulta")
-
-            # Crear un Treeview en la ventana
+            anchos_columnas = {
+                "#0":0,
+                "ID": 30,
+                "Identification": 200,
+                "First Name": 200,
+                "Last Name": 300,
+                "Type Control": 150
+            }
             self.tree_resultado = ttk.Treeview(ventana_resultado, columns=("ID","Identification", "First Name", "Last Name", "Type Control"))
+            self.tree_resultado.config(height=15)
             self.tree_resultado.heading("ID", text="ID")
             self.tree_resultado.heading("Identification", text="Identification")
             self.tree_resultado.heading("First Name", text="First Name")
@@ -66,40 +82,40 @@ class mainMenu:
             self.tree_resultado.heading("Type Control", text="Type Control")
             self.tree_resultado.grid(row=0, column=0, sticky="nsew")
 
-            # Agregar datos al Treeview
+            for col, ancho in anchos_columnas.items():
+                self.tree_resultado.column(col, width=ancho)
+            self.tree_resultado.pack(fill="both", expand=True)
             if respuesta:
-                # Insertar datos en la tabla
                 for index, fila in enumerate(respuesta, start=1):
                     self.tree_resultado.insert("", "end", values=fila)
 
-            self.tree_resultado.bind("<Double-1>", self.on_treeview_click_consulta)
-            # Configurar el Scrollbar
+            self.tree_resultado.bind("<Double-1>", lambda event: self.on_treeview_click(self.tree_resultado))
             scroll_resultado = ttk.Scrollbar(ventana_resultado, orient="vertical", command=self.tree_resultado.yview)
-            scroll_resultado.grid(row=0, column=1, sticky="ns")
+            #scroll_resultado.grid(row=0, column=1, sticky="ns")
             self.tree_resultado.configure(yscrollcommand=scroll_resultado.set)
         else:
             mb.showinfo("Información", "No esite")
-
-    def on_treeview_click_consulta(self, event):
-        item = self.tree_resultado.selection()[0]
-        fila_seleccionada = self.tree_resultado.item(item, "values")
-        #print("Fila seleccionada:", fila_seleccionada)
-        ventana_detalle = tk.Toplevel()
-        ventana_detalle.title("Detalles de Fila")
-        identifications=self.articulo1.identificacionesPersona(fila_seleccionada[0])
-        #print(identifications)
-        self.mostrarIdentifications(identifications,ventana_detalle)
 
     def listado_completo(self):
         self.pagina3 = ttk.Frame(self.cuaderno1)
         self.cuaderno1.add(self.pagina3, text="Listado completo")
 
-        self.tree = ttk.Treeview(self.pagina3, columns=("ID", "First Name", "Type Control","Last Name"))
+        anchos_columnas = {
+                "#0":0,
+                "ID": 40,
+                "First Name": 200,
+                "Last Name": 300,
+                "Type Control": 200
+            }
+        self.tree = ttk.Treeview(self.pagina3, columns=("ID", "First Name", "Last Name","Type Control"))
         self.tree.heading("ID", text="ID")
         self.tree.heading("First Name", text="First Name")
-        self.tree.heading("Type Control", text="Type Control")
         self.tree.heading("Last Name", text="Last Name")
+        self.tree.heading("Type Control", text="Type Control")
         self.tree.grid(row=0, column=0, sticky="nsew")
+
+        for col, ancho in anchos_columnas.items():
+            self.tree.column(col, width=ancho)
 
         self.scroll = ttk.Scrollbar(self.pagina3, orient="vertical", command=self.tree.yview)
         self.scroll.grid(row=0, column=1, sticky="ns")
@@ -107,46 +123,49 @@ class mainMenu:
 
         self.boton1 = ttk.Button(self.pagina3, text="Obtener Todos", command=self.listar)
         self.boton1.grid(row=1, column=0, padx=4, pady=4)
-
+        
     def listar(self):
-        respuesta=self.articulo1.recuperar_todos()
-        # Limpiar la tabla antes de insertar nuevos datos
+        respuesta=self.sanctionActions.recuperar_todos()
         for row in self.tree.get_children():
             self.tree.delete(row)
 
-        # Insertar datos en la tabla
         for index, fila in enumerate(respuesta, start=1):
             self.tree.insert("", "end", values=fila)
+            self.tree.grid(row=0, column=0, sticky="nsew")
 
-        self.tree.bind("<Double-1>", self.on_treeview_click)
+        self.tree.bind("<Double-1>", lambda event: self.on_treeview_click(self.tree))
 
-    def on_treeview_click(self, event):
-        item = self.tree.selection()[0]
-        fila_seleccionada = self.tree.item(item, "values")
-        #print("Fila seleccionada:", fila_seleccionada)
-        ventana_detalle = tk.Toplevel()
-        ventana_detalle.title("Detalles de Fila")
-        identifications=self.articulo1.identificacionesPersona(fila_seleccionada[0])
-        #print(identifications)
-        self.mostrarIdentifications(identifications,ventana_detalle)
+    def on_treeview_click(self, selectedTree):
+        item = selectedTree.selection()[0]
+        fila_seleccionada = selectedTree.item(item, "values")
+        print("Fila seleccionada:", fila_seleccionada)
+        identifications=self.sanctionActions.identificacionesPersona(fila_seleccionada[0])
+        if(len(identifications)<=0):
+            mb.showinfo("Información", "No tiene Identificaciones")
+        else:
+            ventana_detalle = tk.Toplevel()
+            ventana_detalle.title(f"Idenficaciones de:  {fila_seleccionada[0]}")
+        
+            self.mostrarIdentifications(identifications,ventana_detalle)
 
     def mostrarIdentifications(self,identifications,ventana):
+        anchos_columnas = {
+                "#0":40,
+                "Tipo Document": 200,
+                "Documento": 300,
+                "Pais": 150
+            }
+        identificationsView = ttk.Treeview(ventana)
+        identificationsView["columns"] = ( "Tipo Document", "Documento", "Pais")  # Puedes agregar más columnas según sea necesario
+        for col, ancho in anchos_columnas.items():
+            identificationsView.column(col, width=ancho)
+        identificationsView.heading("Tipo Document", text="Tipo Documento")
+        identificationsView.heading("Documento", text="Documento")
+        identificationsView.heading("Pais", text="Pais")
 
-        tree = ttk.Treeview(ventana)
-        if(len(identifications)<=0):
-            texto = tk.Text(ventana)
-            texto.pack()
-            texto.insert(tk.END, "No hay identificaciones.")
-        else:
-            tree["columns"] = ( "Columna1", "Columna2", "Columna3")  # Puedes agregar más columnas según sea necesario
-            tree.heading("#0", text="Índice")
-            tree.heading("Columna1", text="Tipo Documento")
-            tree.heading("Columna2", text="Documento")
-            tree.heading("Columna3", text="Pais")
-
-            for index, identificacion in enumerate(identifications, start=1):
-                tree.insert("", "end", text=str(index), values=[identificacion[1],identificacion[2],identificacion[3]])
-            tree.pack(expand=True, fill="both")
+        for index, identificacion in enumerate(identifications, start=1):
+            identificationsView.insert("", "end", text=str(index), values=[identificacion[1],identificacion[2],identificacion[3]])
+        identificationsView.pack(expand=True, fill="both")
 
 def abrir_ventana_principal():
     ventana_principal = mainMenu()
--- personasSancionadas.py
+++ personasSancionadas.py
@@ -1,20 +1,15 @@
 import psycopg2
-
+import updateControlLists
 class personasSancionadas:
 
     def abrir(self):
         conexion = psycopg2.connect(dbname="postgres", user="postgres", password="admin")
         return conexion
-    
-    def consulta(self, datos):
-        cone=self.abrir()
-        cursor=cone.cursor()
-        sql="select firstname, lastname from personas_sancionadas where firstname=%s"
-        cursor.execute(sql, datos)
-        return cursor.fetchall()
+
+    def updateControlLists(self):
+        updateControlLists.executeScript()
 
     def identificacionesPersona(self,codigo):
-        #print("Codigo: ",codigo)
         cone=self.abrir()
         cursor=cone.cursor()
         sql="select * from personas_sancionadas_identificaciones where id_person=%s"
@@ -22,7 +17,6 @@ class personasSancionadas:
         return cursor.fetchall()
 
     def personLikeIdentification(self,identification):
-        #print("Idenfificacion TEST=> ",identification[0])
         identification=identification[0]
         cone=self.abrir()
         cursor=cone.cursor()
@@ -30,14 +24,13 @@ class personasSancionadas:
           SELECT personas_sancionadas.id,personas_sancionadas_identificaciones.numberidentification,personas_sancionadas.firstname, personas_sancionadas.lastname, personas_sancionadas.typecontrol
             FROM personas_sancionadas_identificaciones
             LEFT JOIN personas_sancionadas ON personas_sancionadas_identificaciones.id_person = personas_sancionadas.id
-            WHERE personas_sancionadas_identificaciones.numberidentification LIKE %s;
+            WHERE personas_sancionadas_identificaciones.numberidentification ILIKE %s;
 
         """
         cursor.execute(sql, ('%' + identification + '%',))
         return cursor.fetchall()
 
     def personLikeName(self,Nombre):
-        #print("Idenfificacion TEST=> ",Nombre[0])
         Nombre=Nombre[0]
         cone=self.abrir()
         cursor=cone.cursor()
@@ -45,7 +38,7 @@ class personasSancionadas:
           SELECT personas_sancionadas.id,personas_sancionadas_identificaciones.numberidentification,personas_sancionadas.firstname, personas_sancionadas.lastname, personas_sancionadas.typecontrol
             FROM personas_sancionadas_identificaciones
             LEFT JOIN personas_sancionadas ON personas_sancionadas_identificaciones.id_person = personas_sancionadas.id
-            WHERE lastname LIKE %s;
+            WHERE lastname ILIKE %s;
 
         """
         cursor.execute(sql, ('%' + Nombre + '%',))
@@ -55,6 +48,6 @@ class personasSancionadas:
     def recuperar_todos(self):
         cone=self.abrir()
         cursor=cone.cursor()
-        sql="select * from personas_sancionadas"
+        sql="select id, firstname, lastname, typecontrol from personas_sancionadas"
         cursor.execute(sql)
         return cursor.fetchall()
